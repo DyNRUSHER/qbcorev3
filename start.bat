9d3cc524644a21292d853b698ae4ed66e2d78ed1
clicmefast <80172192+clicmefast@users.noreply.github.com>
2021-11-07 13:54:09 +0500

    srk
    1st commit

---
+++ start.bat
@@ -0,0 +1,2 @@
+cd C:\Users\Administrator\Desktop\qbcorev3
+C:\Users\Administrator\Desktop\qbcorev3\server/FXServer.exe +exec server.cfg +set onesync legacy
---
+++ start_3456_default.bat
@@ -0,0 +1,3 @@
+@echo off
+F:/qbcorev3/server//FXServer.exe +set serverProfile "default"
+pause
